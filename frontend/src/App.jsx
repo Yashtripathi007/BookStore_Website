765c23c12cec7e6426d5dda34250361e31f46966
Yash Tripathi <tripathiyash700@gmail.com>
2024-10-18 23:19:25 +0530

    backend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Home from './Home/Home';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Courses from './Course/Courses';
 import Signup from './components/Signup';
-
+import { useAuth } from './context/AuthProvider';
 function App() {
+  const [authuser,setAuthUser]=useAuth();
+  console.log(authuser);
   return (
     <>
   {/* <Home/>
        <Course/> */}
-
+ 
        <Routes>
 
        <Route path="/" element={<Home/>}/>
-       <Route path="/Course" element={<Courses/>}/>
+       <Route path="/Course" element={authuser?<Courses/>:<Navigate to="/Signup" />}/>
        <Route path="/Signup" element={<Signup/>}/>
        </Routes>
         
